d4cdd2caf2a6e5542e5b8c074f98065dcebb9065
shilpa <shilpasahlot06@gmail.com>
2019-02-11 05:52:38 -0500

    radio button without "value" attribute

--- src/radioButton.java
+++ src/radioButton.java
@@ -4,12 +4,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class radioButton {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C://INSTALLS//chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.echoecho.com/htmlforms10.htm");
-		driver.findElement(By.xpath("//input[@value='Milk']")).click();
-		System.out.println(driver.findElements(By.xpath("//input[@name='group1']")).size());
+		//driver.findElement(By.xpath("//input[@value='Milk']")).click();
+		
+		Thread.sleep(5000);
+		int count =driver.findElements(By.xpath("//input[@name='group1']")).size();
+		for(int i=0;i<count;i++) 
+		{
+			
+			//driver.findElements(By.xpath("//input[@name='group1']")).get(i).click();
+			
+			//OR
+			
+			String text =driver.findElements(By.xpath("//input[@name='group1']")).get(i).getAttribute("value");
+			if(text.equals("Cheese"))
+			{
+				driver.findElements(By.xpath("//input[@name='group1']")).get(i).click();
+			}
+			
+		}
 
 	}
 
