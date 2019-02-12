0c65e8117ea045e69c5af23a4a08b39f14a6b88d
shilpa <shilpasahlot06@gmail.com>
2019-02-12 11:56:04 -0500

    used assert to check test case pass or fail

--- src/checkBox.java
+++ src/checkBox.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class checkBox {
 
@@ -8,12 +9,20 @@ public class checkBox {
 		System.setProperty("webdriver.chrome.driver", "C://Installs//chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.spicejet.com/");
+		
 		System.out.println(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());  //it will give false in result bcz checkbox is not selected.
+		//OR to check the result is matching the expected result and is passing without checking everytime in console will use assert.
+		Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
+		
 		driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).click();
 		System.out.println(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
+		//OR
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
 		
 		//count total no of checkbox
 		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+		//OR
+		Assert.assertEquals(driver.findElements(By.cssSelector("input[type='checkbox']")).size(), 6);
 		
 		
 	}
