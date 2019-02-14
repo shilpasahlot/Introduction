6369f957cad202b751122008075b1f886033de52
shilpa <shilpasahlot06@gmail.com>
2019-02-14 06:39:55 -0500

    updated

--- src/dynamicDropdown.java
+++ src/dynamicDropdown.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import net.bytebuddy.agent.builder.AgentBuilder.CircularityLock.Default;
 
@@ -18,6 +19,19 @@ public class dynamicDropdown {
 		
 		driver.findElement(By.cssSelector(".ui-state-default.ui-state-highlight.ui-state-active")).click();
 		
+		System.out.println(driver.findElement(By.name("ctl00$mainContent$view_date2")).isEnabled());
+		System.out.println(driver.findElement(By.id("Div1")).getAttribute("style"));
+		driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
+		System.out.println(driver.findElement(By.id("Div1")).getAttribute("style"));
+		if(driver.findElement(By.id("Div1")).getAttribute("style").contains("1"))
+		{
+			System.out.println("is enabled");
+			Assert.assertTrue(true);
+		}
+		else 
+		{
+			Assert.assertTrue(false);
+		}
 	}
 
 }
