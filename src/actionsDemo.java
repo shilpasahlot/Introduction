e8b08e5169e7ad532d4d13784c01bc1d53af5766
shilpa <shilpasahlot06@gmail.com>
2019-02-17 10:45:57 -0500

    Writting in Capital Letters

--- src/actionsDemo.java
+++ src/actionsDemo.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -8,9 +10,16 @@ public class actionsDemo {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C://INSTALLS//chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("https://www.amazon.com/");
 		Actions a = new Actions(driver);
-		a.moveToElement(driver.findElement(By.cssSelector("a[id='nav-link-accountList']"))).build().perform();
+
+		// To write in Caps
+		a.moveToElement(driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
+
+		// Moves to specific element
+		WebElement move = driver.findElement(By.cssSelector("a[id='nav-link-accountList']"));
+		a.moveToElement(move).contextClick().build().perform();
 
 	}
 
