cc282d2fd1acf45a3b66688a5397495b6ca52cc4
shilpa <shilpasahlot06@gmail.com>
2019-02-22 18:24:52 -0500

    ssl certificate

---
+++ src/sslCertificate.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class sslCertificate {
+
+	public static void main(String[] args) {
+		DesiredCapabilities ch = DesiredCapabilities.chrome();
+		ch.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+		//ch.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+		ChromeOptions c = new ChromeOptions();
+		c.merge(ch);
+		System.setProperty("webdriver.chrome.driver", "C://INSTALLS//chromedriver.exe");
+		WebDriver driver = new ChromeDriver(c);
+
+	}
+
+}
